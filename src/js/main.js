96a31c5fc1fc787a24015bf61f923196e40e101f
Bruna <brunadasilvabz98@gmail.com>
2022-11-12 21:28:28 +0100

    add some css

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ console.log("funciona?funciona");
 //QUERYSELECTOR
 
 const cardList = document.querySelector(".js-cardList");
+const searchInput = document.querySelector(".js-input");
 
 //VARIABLES GLOBALES -> VARIABLES CON DATOS DE LA APP
 
@@ -14,18 +15,23 @@ let characters = [];
 function renderOneCard(characters) {
   //DOM AVANZADO PARA PINTAR LAS TARJETAS
   const liElement = document.createElement("li");
+  liElement.setAttribute("class", "listElement");
+
   const articleElement = document.createElement("article");
+  articleElement.setAttribute("class", "listElement__article");
 
   const imageElement = document.createElement("img");
   imageElement.setAttribute("src", characters.img);
-  imageElement.setAttribute("alt", `${characters.img}`);
-  //imageElement.style = `background-image: ${characters.img}`;
+  imageElement.setAttribute("alt", `${characters.name}`);
+  imageElement.setAttribute("class", "listElement__article--img");
 
   const nameElement = document.createElement("h3");
   const nameTextElement = document.createTextNode(`${characters.name}`);
+  nameElement.setAttribute("class", "listElement__article--name");
 
   const statusElement = document.createElement("p");
   const statusTextElement = document.createTextNode(`${characters.status}`);
+  statusElement.setAttribute("class", "listElement__article--status");
 
   nameElement.appendChild(nameTextElement);
   statusElement.appendChild(statusTextElement);
